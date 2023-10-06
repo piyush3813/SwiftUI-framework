5a44dfa47bc486824441f346467eb73adf35a92c
piyushXP0067 <114660663+piyushXP0067@users.noreply.github.com>
2023-10-06 10:53:42 +0530

    Initial Commit

---
+++ SwiftUI-framework/SwiftUI_frameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_frameworkApp.swift
+//  SwiftUI-framework
+//
+//  Created by XP India  on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_frameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
